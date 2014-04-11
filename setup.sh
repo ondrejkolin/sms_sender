35168764beeb941fdb9d3b5a334ff82acd85a170
Ondřej Kolín <ondrej.kolin@linux-mint-czech.cz>
2014-04-11 14:37:49 +0200

    Reedited paths

---
+++ setup.sh
@@ -0,0 +1,10 @@
+echo "Pokud nemáte administrátorská oprávnění skript selže!"
+echo "Zadejte vaše heslo pro instalaci smssender"
+sudo apt-get install sqlite python-sqlite glade
+sudo mkdir /usr/bin/smssender
+echo "Probíhá registrace do /usr/bin"
+sudo cp ./usr/bin/smssender /usr/bin/smssender
+sudo mkdir /usr/share/smssender
+echo "Probíhá kopírování knihoven"
+sudo cp -r ./usr/share/smssender /usr/share/smssender
+
